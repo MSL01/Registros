25bf5879d5d77da04cdbb5bdfab90859b8adc30e
MSL01 <miguel.saavedra@utp.edu.co>
2023-02-13 00:29:09 -0500

    primera2

---
+++ Createdata.py
@@ -0,0 +1,72 @@
+from tkinter import Tk
+import numpy as np
+import pandas as pd
+from tkinter import messagebox
+from tkinter import ttk
+from tkinter import *
+from tkcalendar import  DateEntry
+
+def main2():
+    root = Tk()
+    gui = CreateData(root)
+    gui.root.mainloop()
+    return None
+
+class CreateData:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("524x514")
+        self.root.title('Formulario de registro de datos')
+        self.root.resizable(False, False)
+
+        self.nombre_label = ttk.Label(self.root, text='NOMBRE DEL EVENTO')
+        self.nombre_label.place(x=20,y=20)
+
+        self.nombre_ev = Entry(self.root, width=80)
+        self.nombre_ev.place(x=20,y=70)
+
+        self.tipo_label = ttk.Label(self.root, text='TIPO DE EVENTO')
+        self.tipo_label.place(x=20, y=110)
+
+        self.tipo_ev = Entry(self.root, width=80)
+        self.tipo_ev.place(x=20, y=160)
+
+        self.desc_label = ttk.Label(self.root, text='DESCRIPCIÓN DEL EVENTO')
+        self.desc_label.place(x=20, y=200)
+
+        self.desc_ev = Text(self.root, height = 7, width = 60)
+        self.desc_ev.place(x=20,y=250)
+
+        self.fecha_label = ttk.Label(self.root, text='FECHA DEL EVENTO')
+        self.fecha_label.place(x=20, y=380)
+
+        self.fecha_ev = DateEntry(self.root, selectmode='day', width=40)
+        self.fecha_ev.place(x=20,y=420)
+
+        self.registrar_data = ttk.Button(self.root, text='Registrar datos', command=self.registrar_data)
+        self.registrar_data.place(x=390, y=460)
+        #self.data_pd = {'Nombre del evento': [], 'Tipo de evento': [],
+        #                'Descripcion': [], 'Fecha': [], 'Estado': [],
+        #                'Gestion': []
+        #                }
+        #self.df = pd.DataFrame(self.data_pd)
+        self.df = pd.read_csv((r'C:\Users\migue\OneDrive\Escritorio\RegistroGUI\dataframe.csv'), index_col=[0])
+    def registrar_data(self, event=None):
+        new_data = {'Nombre del evento': self.nombre_ev.get(), 'Tipo de evento': self.tipo_ev.get(),
+                        'Descripcion': self.desc_ev.get("1.0",'end-1c'), 'Fecha': self.fecha_ev.get(), 'Estado': 'Sin revisar',
+                        'Gestion': ''
+                        }
+        self.df = pd.concat([self.df, pd.DataFrame(
+            {'Nombre del evento': self.nombre_ev.get(), 'Tipo de evento': self.tipo_ev.get(),
+             'Descripcion': self.desc_ev.get("1.0", 'end-1c'), 'Fecha': self.fecha_ev.get(), 'Estado': 'Sin revisar',
+             'Gestion': ''
+             }, index=[0]
+        )], ignore_index=True)
+        self.df.update(self.df)
+        self.df.to_csv(r'C:\Users\migue\OneDrive\Escritorio\RegistroGUI\dataframe.csv')
+        messagebox.showinfo('Registro exitoso!', 'Información ingresada')
+        self.root.withdraw()
+
+
+if __name__ == "__main__":
+    main2()
---
+++ model_menu.py
@@ -0,0 +1,65 @@
+from tkinter import Tk
+import numpy as np
+import pandas as pd
+from tkinter import ttk
+from tkinter import *
+import Createdata as create
+
+def main():
+    root = Tk()
+    gui = ModelMenu(root)
+    gui.root.mainloop()
+    return None
+
+
+class ModelMenu:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("413x416")
+        self.root.title("Registro de eventos con python GUI")
+        self.root.resizable(False, False)
+        #--------------------------registros------------------------
+        self.df = pd.read_csv((r'C:\Users\migue\OneDrive\Escritorio\RegistroGUI\dataframe.csv'), index_col=[0])
+
+        # ---------------------Eventos No revisados ------------------------
+        self.title_no_revisados = ttk.Label(self.root, text='EVENTOS SIN REVISAR', font="bold")
+        self.title_no_revisados.place(x=100, y=160)
+
+        if 'Sin revisar' in self.df.Estado.values:
+            self.select_no_revisado = ttk.Combobox(self.root, values=[self.df['Nombre del evento'].values[i] for i in range(self.df.columns.size)] , width=59)
+            self.select_no_revisado.place(x=20, y=230)
+        else:
+            self.select_no_revisado = ttk.Combobox(self.root, values='', width=59)
+            self.select_no_revisado.place(x=20, y=230)
+
+        self.ver_no_revisado = ttk.Button(self.root, text='Ver')
+        self.ver_no_revisado.place(x=160, y=270)
+
+        # ---------------------Eventos revisados ------------------------
+        self.title_revisados = ttk.Label(self.root, text='EVENTOS REVISADOS', font="bold")
+        self.title_revisados.place(x=100, y=20)
+
+        if 'Revisado' in self.df.Estado.values:
+            self.select_revisado = ttk.Combobox(self.root, values=[self.df['Nombre del evento'].values[i] for i in range(self.df.columns.size)], width=59)
+            self.select_revisado.place(x=20, y=90)
+        else:
+            self.select_revisado = ttk.Combobox(self.root, width=59)
+            self.select_revisado.place(x=20, y=90)
+
+        self.ver_revisado = ttk.Button(self.root, text='Ver')
+        self.ver_revisado.place(x=160, y=120)
+
+        #-----------------------Registrar eventos-------------------------
+        self.registrar = ttk.Button(self.root, text='Registrar un evento', command=gui_registrar_data)
+        self.registrar.place(x=10,y=370)
+
+        # -----------------------Actualizar eventos-------------------------
+        self.registrar = ttk.Button(self.root, text='Actualizar')
+        self.registrar.place(x=290, y=370)
+
+def gui_registrar_data(event=None):
+    create.main2()
+    pass
+
+if __name__ == "__main__":
+    main()
